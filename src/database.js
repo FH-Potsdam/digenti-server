ce6d19ed62934d31f273c9ea039e8f76bc1c2d30
rpu <rpu@b306987c-6475-4904-9b0a-7407a53edcd8>
2016-02-10 15:45:01 +0000

    Implemented maxSize and q search
    git-svn-id: svn://geoway.de/digenti@14 b306987c-6475-4904-9b0a-7407a53edcd8

--- src/database.js
+++ src/database.js
@@ -1,4 +1,5 @@
 /*global require,module*/
+var R = require("ramda");
 var D = require("./digenti");
 var pg = D.require("pg");
 var config = D.require("./config");
@@ -19,5 +20,15 @@ var query = function(sql, resultFun){
     });
   });
 };
+
+var ilike = R.curry(function(column,str){ return column+" ilike '%"+D.quote_sql(str.trim())+"%'";});
+var or = R.join(" OR ");
+var ilike_column = function(column){ return R.pipe(R.map(ilike(column)),or); };
+var csv_text = R.split(/\s*,s*/);
+
 // Module definitions
 module.exports.query = query;
+module.exports.ilike = ilike;
+module.exports.ilike_column = ilike_column;
+module.exports.or = or;
+module.exports.csv_text = csv_text;
--- src/search/server.js
+++ src/search/server.js
@@ -1,5 +1,6 @@
 /*global require*/
 // REST API Search Server
+var R = require("ramda");
 var D = require("../digenti");
 var db = D.require("./database");
 var fs = require('fs');
@@ -26,14 +27,29 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 //////////////
 // REST API
 //////////////
+var defaultQuery = {
+  maxResults: 50,
+  location: "27.7167,85.3667",
+  locationRadius: "300km",
+  publishedAfter: "2015-04-24T00:00:00Z",
+  publishedBefore: "2015-06-01T00:00:00Z",
+  "q": ""
+};
 
 var search = function(req,res){
-  db.query("SELECT * FROM "+req.body.table+";",function(resultSet){
-    res.json({query:req.body, count:resultSet.rowCount,records:resultSet.rows}); 
+  var query = R.merge(defaultQuery,req.body);
+  D.trace("query:",query);
+  var keywords = db.csv_text(query.q);
+  var ilike_title = db.ilike_column("requesttitle");
+  var ilike_desc = db.ilike_column("description");
+  var sql = "SELECT * FROM search_items WHERE "
+            +ilike_title(keywords)+" OR "+ilike_desc(keywords)
+            +" LIMIT "+query.maxResults+";";
+  db.query(sql,function(resultSet){
+    res.json({dev:{ sql: sql},query:req.body, count:resultSet.rowCount,records:resultSet.rows}); 
   });
 };
 //app.get("/search",search);
