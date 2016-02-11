dcbe72f10f88f70747f4c4c909598d91d10d13a0
rpu <rpu@b306987c-6475-4904-9b0a-7407a53edcd8>
2016-02-11 10:13:00 +0000

    Fixed minor js warnings
    git-svn-id: svn://geoway.de/digenti@20 b306987c-6475-4904-9b0a-7407a53edcd8

--- src/aidr_twitter/request.js
+++ src/aidr_twitter/request.js
@@ -7,7 +7,7 @@ var client = new Twitter({
   consumer_key: config.twitter.consumer_key,
   consumer_secret: config.twitter.consumer_secret,
   access_token_key: config.twitter.access_token_key,
-  access_token_secret: config.twitter.access_token_secret,
+  access_token_secret: config.twitter.access_token_secret
 });
 
 var get = function(params){
